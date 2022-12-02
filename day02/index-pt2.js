39c3ac849e3bffc420fbddeca136ccc5c200a057
Benjamin Lannon <benjamin@lannonbr.com>
2022-12-02 00:36:47 -0500

    Day 02

---
+++ day02/index-pt2.js
@@ -0,0 +1,48 @@
+const fs = require("fs");
+const data = fs.readFileSync("./input.txt").toString();
+
+/**
+ * A Rock
+ * X Rock
+ *
+ * B Paper
+ * Y Paper
+ *
+ * C Scissors
+ * Z Scissors
+ *
+ * win = rock scissors
+ * win = paper rock
+ * win = scissors paper
+ *
+ * C X
+ * A Y
+ * B Z
+ */
+
+let n = data
+  .split("\n")
+  .map((a) => {
+    if (a == "A Y") {
+      return 3 + 1;
+    } else if (a == "C X") {
+      return 0 + 2;
+    } else if (a == "B Z") {
+      return 6 + 3;
+    } else if (a == "A X") {
+      return 0 + 3;
+    } else if (a == "B Y") {
+      return 3 + 2;
+    } else if (a == "C Z") {
+      return 6 + 1;
+    } else if (a == "A Z") {
+      return 6 + 2;
+    } else if (a == "B X") {
+      return 0 + 1;
+    } else if (a == "C Y") {
+      return 3 + 3;
+    }
+  })
+  .reduce((acc, b) => acc + b, 0);
+
+console.log(n);
